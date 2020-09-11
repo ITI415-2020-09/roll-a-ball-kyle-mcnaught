f5b9c5fcb89d5f3032a456fc8c0652dc551a02af
kyle-mcnaught <60240503+kyle-mcnaught@users.noreply.github.com>
2020-09-10 23:00:25 -0400

    Added Counter
    Counter on top left of the screen counts how many of the cubes the player has picked up.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
 
 	public float speed = 0;
+	public TextMeshProUGUI countText;
 
 	private Rigidbody rb;
+	private int count;
 	private float movementX;
 	private float movementY;
 
@@ -16,6 +19,9 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
       rb = GetComponent<Rigidbody>();  
+      count = 0;
+      
+      SetCountText();
     }
 
    void OnMove(InputValue movementValue)
@@ -26,6 +32,10 @@ public class PlayerController : MonoBehaviour
    	movementY = movementVector.y;
    }
    
+   void SetCountText ()
+   {
+   countText.text = "Count: " + count.ToString();
+   }
    
    void FixedUpdate()
    {
@@ -39,6 +49,9 @@ public class PlayerController : MonoBehaviour
    if(other.gameObject.CompareTag("Pickup"))
    {
       	other.gameObject.SetActive(false);
+      	count = count + 1;
+      	
+      	 SetCountText();
    }
    }
 }
